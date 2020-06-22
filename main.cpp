5abe55c6dac11783b4bc58560c45b4a5ec099e58
AKASH MAJI <akashmaji946@gmail.com>
2020-06-22 08:59:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+Date: 22 June 2020
+Author: Akash Maji
+Program: To find the factorial a given number(not to be large)
+*/
+
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+
+  if(n < 0) return -1;
+  else if(n == 0) return 1;
+  else return n * fact(n-1);
+
+}
+
+int main() {
+  std::cout << "Hello World!\n";
+  int number;
+  cout << "Enter a number:";
+  cin >> number;
+  cout << fact(number);
+  cout << endl;
+  return 0;
+
+}
